0a1418341a301c78ffaf68951678a073344b6de7
Aloisio Ribeiro <aloisio@users.noreply.github.com>
2017-10-19 21:40:06 -0400

    Setup integration script, fail build if lint fails

--- Jakefile.js
+++ Jakefile.js
@@ -1,11 +1,11 @@
-/* global desc, task, jake */
+/* global desc, task, jake, fail */
 (function () {
     'use strict';
 
+    desc('Build and test');
     task('default', ['lint']);
 
     desc('Lint everything');
-
     task('lint', [], function () {
         var lint = require('./build/lint/lint_runner.js');
 
@@ -13,7 +13,22 @@
         files.include('**/*.js');
         files.exclude('node_modules');
 
-        lint.validateFileList(files.toArray(), nodeLintOptions(), {});
+        if (!lint.validateFileList(files.toArray(), nodeLintOptions(), {})) {
+            fail('Lint failed');
+        }
+    });
+
+    desc("Integrate");
+    task("integrate", ["default"], function () {
+        console.log("1. Make sure 'git status' is clean.");
+        console.log("2. Build on the integration box.");
+        console.log("   a. Walk over to integration box.");
+        console.log("   b. 'git pull'");
+        console.log("   c. 'jake'");
+        console.log("   d. If jake fails, stop! Try again after fixing the issue.");
+        console.log("3. 'git checkout integration'");
+        console.log("4. 'git merge master --no-ff --log'");
+        console.log("5. 'git checkout master'");
     });
 
     function nodeLintOptions() {
